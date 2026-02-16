3ef5b1b63b3dedce040767c623c0db4ffda34da7
Alexandru Cristian Stinga Micu <astinmic@myuax.com>
2026-02-16 20:24:18 +0100

    commit

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-app = FastAPI()
+
+app = FastAPI(title="SkySentinel API")
 
 @app.get("/")
 def root():
-    return {"message": "SkySentinel Backend operando"}
+    return {"message": "SkySentinel Backend operando con Python 3.13"}
