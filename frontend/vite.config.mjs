fdb1817edecc62fe3a7367d998d61873561ad4e4
Alexandru Cristian Stinga Micu <astinmic@myuax.com>
2026-02-16 20:41:09 +0100

    Commit del tailwind actuualizado

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,9 +1,13 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite" // <--- Añade esto
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // <--- Y añade esto aquí
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
